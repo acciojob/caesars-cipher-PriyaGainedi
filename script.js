9a3328e79236cc7e4480d653bedffd07a302c180
Monika Priya Gainedi <priyagainedi8@gmail.com>
2023-03-30 18:47:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,43 @@
 // Your Script here.
-console.log(char(97));
+var ROT13 = {'A':'N',
+			'B':'O',
+			'C':'P',
+			'D':'Q',
+			'E':'R',
+			'F':'S',
+			'G':'T',
+			'H':'U',
+			'I':'V',
+			'J':'W',
+			'K':'X',
+			'L':'Y',
+			'M':'Z',
+			'N':'A',
+			'O':'B',
+			'P':'C',
+			'Q':'D',
+			'R':'E',
+			'S':'F',
+			'T':'G',
+			'U':'H',
+			'V':'I',
+			'W':'J',
+			'X':'K',
+			'Y':'L',
+			'Z':'M'};
+function rot13(encodedStr) {
+  var words = encodedStr.split(" "); // String to Array
+  let decodedArr = []; // Your Result goes here
+  // Only change code below this line
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+    let decoded_word = "";
+    for (let j = 0; j < word.length; j++) {
+      var char = word.charAt(j);
+      var decoded_char = lookup[char];
+      decoded_word += decoded_char;
+    }
+    decodedArr.push(decoded_word);
+  }
+  return decodedArr.join(" ");
+}
